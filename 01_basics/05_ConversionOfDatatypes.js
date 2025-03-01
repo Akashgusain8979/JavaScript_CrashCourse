a72f8b65141e2f0abcc1553549e3cd39bdbd6908
Akash <akashgusain13@gmail.com>
2025-03-01 08:08:46 +0530

    Conversion of Data types

---
+++ 01_basics/05_ConversionOfDatatypes.js
@@ -0,0 +1,53 @@
+let score = 33;
+let score1 = "33";
+let score2 = "33abc";
+let score3 = null;
+let score4 = undefined;
+let score5 = true;
+let score6 = false;
+
+// console.log(typeof score);
+// console.log(typeof score1);
+
+// let valueInNumber = Number(score1);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber = Number(score2);
+// console.log(typeof score2);
+// console.log(valueInNumber); // NaN
+
+// let valueInNumber = Number(score3);
+// console.log(typeof score3);
+// console.log(valueInNumber); // 0
+
+// let valueInNumber = Number(score4);
+// console.log(typeof score4);
+// console.log(valueInNumber); // Nan
+
+// let valueInNumber = Number(score5);
+// console.log(typeof score5);
+// console.log(valueInNumber); // 1
+
+// let valueInNumber = Number(score6);
+// console.log(typeof score6);
+// console.log(valueInNumber); // 0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggenIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggenIn); // true
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggenIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggenIn); // false
+
+// let isLoggedIn = "Akash";
+
+// let booleanIsLoggenIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggenIn); // true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
