3d2a5d89ddd13d85431679363a41c1b758ebf5a9
Akash <akashgusain13@gmail.com>
2025-03-03 18:02:05 +0530

    String

---
+++ 01_basics/09_String.js
@@ -0,0 +1,9 @@
+let name = "Akshat"; // string declaration
+let score = 50;
+
+console.log(name + score);
+
+console.log(`Hello my name is ${name}, and my score is ${score}`); //Modern way to write string interpolation
+
+const gameName = new String("Mario"); // string declaration
+console.log(gameName);
